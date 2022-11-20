97f3b80c27a9a227db2ac0a85db9dc34ac00d500
Aaron Escoboza <EscobozaAaron@JohnDeere.com>
2022-11-19 19:33:36 -0600

    add option to calculate the update event period (period value)

--- tim_cal.py
+++ tim_cal.py
@@ -7,7 +7,7 @@
 
 import sys
 import getopt
-
+import math
 class Stm32TimCalulator:
     defaultTimxClk = 16000000
     defaultPrescaler = 0
@@ -26,22 +26,36 @@ class Stm32TimCalulator:
             prescaler = prescaler + 1
             cnt_clk = float(timxClk) / (prescaler + 1) #Timer count clock
             pulse = time_base / (1 / cnt_clk);
-            print("Debug: prescaler = {}, cnt_clk = {}, period = {}" .format(prescaler, cnt_clk, pulse))
+            print("Debug: prescaler = {}, cnt_clk = {}, pulse = {}" .format(prescaler, cnt_clk, pulse))
         print("Results: Prescaler = {}, Pulse = {}".format(prescaler, pulse))
 
+    def calculatePeriod(self, timxClk, prescaler, updateEventPeriod):
+        prescaler = 0
+        periodAutoReload = 0
+        updateEventPeriod = float(updateEventPeriod) / 1000
+        while True:
+            print("Results: Prescaler = {}, Period = {}".format(prescaler, periodAutoReload))
+            counterClk = float(timxClk) / (prescaler + 1)
+            periodCounterClk = 1 / counterClk
+            periodAutoReload = updateEventPeriod / periodCounterClk
+            if ((periodAutoReload > 0) and (str(periodAutoReload).endswith('.0')) and (periodAutoReload < self.maxCounterDecVal)):
+                break
+            prescaler = prescaler + 1
+        print("Results: Prescaler = {}, Period = {}".format(prescaler, periodAutoReload))
 
 def help():
     print("Debug: Function {}".format(help.__name__))
     print("\n\n\t\t\tAvailable commands:\n")
     print("\t" + "-t            TIMx CLK \n")
-    print("\t" + "-f            Desired channel frequency\n")
+    print("\t" + "-f            OC toggle mode: Desired channel frequency. Return pulse value\n")
+    print("\t" + "-u            Base unit: Desired update event. Return period value")
 
 def runApp():
     argv = sys.argv[1:]
     print("Debug: Function {}".format(runApp.__name__))
 
     try:
-        options , args = getopt.getopt(argv, "t:f:h:l:")
+        options , args = getopt.getopt(argv, "t:f:h:l:u:")
     except:
         print("Internal error")
 
@@ -49,11 +63,14 @@ def runApp():
     print(args)
     for option, arg in options:
         if option in ['-t']:
-            print("Debug: Timer frequency option")
+            print("Debug: Option: Timer frequency option")
             timFreq = arg
         elif option in ['-f']:
-            print("Debug: Desired channel frequency")
+            print("Debug: Option: Desired channel frequency")
             desiredChannelPeriod = 1 / float(arg);
+        elif option in ['-u']:
+            print("Debug: Option: Update event")
+            updateEventPeriod = arg
         elif option in ['h','--help']:
             print("Debug: Help option")
         else:
@@ -61,7 +78,8 @@ def runApp():
             help()
 
     calculator =  Stm32TimCalulator()
-    calculator.calculatePulse(timFreq, desiredChannelPeriod)
-    print("Debug: Results: Timer frequency = {}, Desired channel period = {}".format(timFreq, desiredChannelPeriod))
+    calculator.calculatePeriod(timFreq, 0, updateEventPeriod)
+    #calculator.calculatePulse(timFreq, desiredChannelPeriod)
+    # print("Debug: Results: Timer frequency = {}, Desired channel period = {}".format(timFreq, desiredChannelPeriod))
 
 runApp()
